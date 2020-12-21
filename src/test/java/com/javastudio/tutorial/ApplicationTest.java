7dcfbe51407d372d57406028f5d86278767b074b
Hossein Mohammadi <hussein.muhammadii@gmail.com>
2020-12-21 13:44:56 +0330

    Reactive mutiny

--- src/test/java/com/javastudio/tutorial/ApplicationTest.java
+++ src/test/java/com/javastudio/tutorial/ApplicationTest.java
@@ -1,13 +1,5 @@
 package com.javastudio.tutorial;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 class ApplicationTest {
 
-    @Test
-    void name() {
-
-    }
 }
